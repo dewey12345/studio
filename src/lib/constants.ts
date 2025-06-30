2574720dbde70030720c357c3ae4f62217648504
Debyendu Pal <debyendu.pal337@gmail.com>
2025-06-30 18:46:10 +0000

    Upgrade to Firestore and implement multiple fixes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 
 export const CURRENT_USER_KEY = 'color_clash_current_user';
 export const ROUND_STATE_KEY = 'color_clash_round_state';
+export const GLOBAL_ROUND_HISTORY_KEY = 'color_clash_global_round_history';
